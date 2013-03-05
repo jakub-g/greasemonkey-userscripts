9dc1b572cd630b172d9094140fb7cc92dab0701c
jakub-g <jakub.gieryluk@amadeus.com>
2013-03-05 12:30:33 +0100

    GH togglable diff +chrome

--- github/togglableDiff.js
+++ github/togglableDiff.js
@@ -2,11 +2,11 @@
 // @name            GitHub code review assistant
 // @description     Toggle diff visibility per file in the commit. Mark reviewed files. Useful to review commits with lots of files changed.
 // @icon            https://github.com/favicon.ico
-// @version         0.4.1-20130212
+// @version         0.5.0-20130305
 // @namespace       http://jakub-g.github.com/
 // @author          http://jakub-g.github.com/
 // @downloadURL     https://raw.github.com/jakub-g/greasemonkey-userscripts/master/github/togglableDiff.js
-// @userscriptsOrg  http://userscripts.org/scripts/show/...
+// @userscriptsOrg  http://userscripts.org/scripts/show/153049
 // @grant           none
 // @include         http*://github.com/*/*/commit/*
 // @include         http*://github.com/*/*/pull/*
@@ -32,6 +32,8 @@
 //  accomodated to new GH HTML markup
 // 0.4.1-20130212
 //  enabled also on /compare/ URLs
+// 0.5.0-20130305
+//  Works also in Chrome (Tampermonkey) now!
 
 // ============================= CONFIG ================================
 
@@ -49,73 +51,86 @@ var dontHideUnlessMoreThanFiles = 2;
 var enableReviewedButton = true;
 // ============================== CODE =================================
 
+var freeze = function (collection) {
+    return collection;
+
+    //var output = [];
+    //for(var i=0; i<collection.length; i++){
+    //    output.push(collection[i]);
+    //}
+    //return output;
+};
+
+/**
+ * @param elem element to be toggled upon clicking
+ * @param bStrictTarget whether the event listener should fire only on its strict target or also children
+ */
+var getHandler = function(elem, bStrictTarget) {
+    return function(evt){
+        if(bStrictTarget){
+            if (evt.currentTarget != evt.target) {
+                // don't want to trigger the event when clicking on "View file" or "Show comment"
+                return;
+            }
+        }
+
+        var currDisplay = elem.style.display;
+        if(currDisplay === 'none') {
+            elem.style.display = 'block';
+        } else {
+            elem.style.display = 'none';
+        }
+    };
+};
+
 var attachListeners = function() {
 
-    /**
-     * @param elem element to be toggled upon clicking
-     * @param bStrictTarget whether the event listener should fire only on its strict target or also children
-     */
-    let getHandler = function(elem, bStrictTarget) {
-        return function(evt){
-            if(bStrictTarget){
-                if (evt.currentTarget != evt.target) {
-                    // don't want to trigger the event when clicking on "View file" or "Show comment"
-                    return;
-                }
-            }
+    var mainDiffDiv = document.getElementById('files');
+    var children = freeze(mainDiffDiv.children);
+    var nbOfCommits = children.length;
 
-            let currDisplay = elem.style.display;
-            if(currDisplay === 'none') {
-                elem.style.display = 'block';
-            } else {
-                elem.style.display = 'none';
-            }
-        };
-    };
-
-    let mainDiffDiv = document.getElementById('files');
-    let children = mainDiffDiv.children;
-    let nbOfCommits = children.length;
-
-    // dev note: let vs var is crucial below here in the loop, as 'children' is a live collection...
-    for(let i=0, ii = nbOfCommits; i<ii; i++) {
-        let child = children[i];
-        if(!child.id || child.id.indexOf('diff-') == -1){
-            continue;
-        }
-        let diffContainer = child; // document.getElementById('diff-1');
-
-        // We want the evt to fire on the header and some, but not all of the children...
-        let diffContainerHeader = diffContainer.children[0];
-        let diffContainerFileNameHeader = diffContainerHeader.children[0];
-
-        let diffContainerBody = diffContainer.children[1];
-
-        let handler1 = getHandler(diffContainerBody, false);
-        let handler2 = getHandler(diffContainerBody, true);
-
-        diffContainerFileNameHeader.addEventListener('click', handler1, false);
-        diffContainerHeader.addEventListener('click', handler2, true);
-        diffContainerHeader.style.cursor = 'pointer';
+    for(var i=0, ii = nbOfCommits; i<ii; i++) {
+        var child = children[i];
+        attachListenersToChild(child);
     }
 };
 
+var attachListenersToChild = function (child) {
+    if(!child.id || child.id.indexOf('diff-') == -1){
+        return;
+    }
+    var diffContainer = child; // document.getElementById('diff-1');
+
+    // We want the evt to fire on the header and some, but not all of the children...
+    var diffContainerHeader = diffContainer.children[0];
+    var diffContainerFileNameHeader = diffContainerHeader.children[0];
+
+    var diffContainerBody = diffContainer.children[1];
+
+    var handler1 = getHandler(diffContainerBody, false);
+    var handler2 = getHandler(diffContainerBody, true);
+
+    diffContainerFileNameHeader.addEventListener('click', handler1, false);
+    diffContainerHeader.addEventListener('click', handler2, true);
+    diffContainerHeader.style.cursor = 'pointer';
+}
+
 var toggleDisplayAll = function(bVisible) {
 
-    let mainDiffDiv = document.getElementById('files');
-    let children = mainDiffDiv.children;
-    let nbOfCommits = children.length;
+    var mainDiffDiv = document.getElementById('files');
+    var children = mainDiffDiv.children;
+    var nbOfCommits = children.length;
 
-    let newDisplay = bVisible ? 'block' : 'none';
+    var newDisplay = bVisible ? 'block' : 'none';
 
     for(var i=0, ii = nbOfCommits; i<ii; i++) {
-        let child = children[i];
+        var child = children[i];
         if(!child.id || child.id.indexOf('diff-') == -1){
             continue;
         }
 
-        let diffContainer = child;
-        let diffContainerBody = diffContainer.children[1];
+        var diffContainer = child;
+        var diffContainerBody = diffContainer.children[1];
 
         diffContainerBody.style.display = newDisplay;
     }
@@ -123,20 +138,20 @@ var toggleDisplayAll = function(bVisible) {
 
 var hideLong = function(minDiff) {
 
-    let mainDiffDiv = document.getElementById('files');
-    let children = mainDiffDiv.children;
-    let nbOfCommits = children.length;
+    var mainDiffDiv = document.getElementById('files');
+    var children = freeze(mainDiffDiv.children);
+    var nbOfCommits = children.length;
 
     for(var i=0, ii = nbOfCommits; i<ii; i++) {
-        let child = children[i];
+        var child = children[i];
         if(!child.id || child.id.indexOf('diff-') == -1){
             continue;
         }
 
-        let diffContainer = child;
-        let diffContainerBody = diffContainer.children[1];
+        var diffContainer = child;
+        var diffContainerBody = diffContainer.children[1];
 
-        let diffStats = parseInt(diffContainer.children[0].children[0].children[0].firstChild.textContent, 10);
+        var diffStats = parseInt(diffContainer.children[0].children[0].children[0].firstChild.textContent, 10);
         //console.log(diffStats);
 
         if(diffStats > minDiff){
@@ -156,7 +171,7 @@ var attachToggleButton = function (hiddenByDefault) {
 
     newButton.innerHTML = hiddenByDefault ? 'Expand all' : 'Collapse all';
 
-    let nowHidden = hiddenByDefault; // closure to keep state
+    var nowHidden = hiddenByDefault; // closure to keep state
     newButton.addEventListener('click', function(evt) {
         if(nowHidden == true){
             toggleDisplayAll(true);
@@ -171,65 +186,71 @@ var attachToggleButton = function (hiddenByDefault) {
 
     buttonBar.appendChild(newButton);
 };
+
+var XPathTools = {
+  getElementByXpath : function(xpath, referenceNode) {
+     var xPathResult = document.evaluate (xpath, referenceNode, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null);
+     return xPathResult.singleNodeValue;
+  }
+};
+
 var attachReviewedButton = function () {
 
-   var XPathTools = {
-      getElementByXpath : function(xpath, referenceNode) {
-         var xPathResult = document.evaluate (xpath, referenceNode, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null);
-         return xPathResult.singleNodeValue;
-      }
-   };
-
-    let mainDiffDiv = document.getElementById('files');
-    let children = mainDiffDiv.children;
-    let nbOfCommits = children.length;
+    var mainDiffDiv = document.getElementById('files');
+    var children = freeze(mainDiffDiv.children);
+    var nbOfCommits = children.length;
 
     for(var i=0, ii = nbOfCommits; i<ii; i++) {
-        let child = children[i];
-        if(!child.id || child.id.indexOf('diff-') == -1){
-            continue;
-        }
-
-        let diffContainer = child;
-        let diffContainerHeader = diffContainer.children[0];
-        let diffContainerBody = diffContainer.children[1];
-
-        var parent = XPathTools.getElementByXpath('.//div[@class="actions"]/div[@class="button-group"]', diffContainer);
-        console.log(parent);
-
-
-        var newButton = document.createElement('a');
-        newButton.className = 'minibutton';
-        //newButton.href = '#fakeHash';
-
-        newButton.innerHTML = 'Reviewed';
-
-        let reviewed = false; // closure to keep state
-        newButton.addEventListener('click', function(evt) {
-            if(reviewed == true){
-                reviewed = false;
-                diffContainerHeader.style.backgroundImage = 'linear-gradient(#FAFAFA, #EAEAEA)';
-                diffContainerHeader.style.color = '#555555';
-            } else {
-                reviewed = true;
-                diffContainerHeader.style.backgroundImage = 'linear-gradient(#333, #444)';
-                diffContainerHeader.style.color = '#FFF';
-                diffContainerBody.style.display = 'none';
-            }
-        });
-
-        parent.insertBefore(newButton, parent.firstChild);
+        var child = children[i];
+        attachReviewedButtonChild(child);
     }
+};
+
+var attachReviewedButtonChild = function (child) {
+
+    if(!child.id || child.id.indexOf('diff-') == -1){
+        return;
+    }
+
+    var diffContainer = child;
+    var diffContainerHeader = diffContainer.children[0];
+    var diffContainerBody = diffContainer.children[1];
+
+    var parent = XPathTools.getElementByXpath('.//div[@class="actions"]/div[@class="button-group"]', diffContainer);
+
+    var newButton = document.createElement('a');
+    newButton.className = 'minibutton';
+    //newButton.href = '#fakeHash';
+
+    newButton.innerHTML = 'Reviewed';
+
+    var reviewed = false; // closure to keep state
+    newButton.addEventListener('click', function(evt) {
+        if(reviewed == true){
+            reviewed = false;
+            diffContainerHeader.style.backgroundImage = '-webkit-linear-gradient(top, #FAFAFA, #EAEAEA)';
+            diffContainerHeader.style.backgroundImage = 'linear-gradient(#FAFAFA, #EAEAEA)';
+            diffContainerHeader.style.color = '#555555';
+        } else {
+            reviewed = true;
+            diffContainerHeader.style.backgroundImage = '-webkit-linear-gradient(top, #333, #444)';
+            diffContainerHeader.style.backgroundImage = 'linear-gradient(#333, #444)';
+            diffContainerHeader.style.color = '#FFF';
+            diffContainerBody.style.display = 'none';
+        }
+    });
+
+    parent.insertBefore(newButton, parent.firstChild);
 };
 
 var main = function () {
 
     // read config
-    let mainDiffDiv = document.getElementById('files');
-    let nbOfFiles = mainDiffDiv.children.length;
+    var mainDiffDiv = document.getElementById('files');
+    var nbOfFiles = mainDiffDiv.children.length;
 
-    let autoHide = false;
-    let autoHideLong = false;
+    var autoHide = false;
+    var autoHideLong = false;
     if(nbOfFiles > dontHideUnlessMoreThanFiles) {
         if(hideAllWhenMoreThanFiles > 0 && nbOfFiles > hideAllWhenMoreThanFiles){
             autoHide = true;
